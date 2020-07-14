aa4b5fa6daf376ea0e2db7cea927926ae0ca325b
Richard Chotvac <chotvacr@gmail.com>
2020-07-14 22:15:55 +0200

    css

--- resources/js/App/Components/activities.jsx
+++ resources/js/App/Components/activities.jsx
@@ -60,23 +60,32 @@ export default class ActivityList extends React.Component {
 
         return (
            <>
-            <div className="activity-list-detail">
-                {
-                    this.state.data.map(activity => (
+           <div className="flexdisplay1">
+            <div className="flexdisplay">
+                    <div className="activity-list-detail">
+                        {
+                            this.state.data.map(activity => (
 
-                        <div className="activity" key={ activity.id }>
-                            
-                            <div className="activity__name">{ activity.name }</div>
-                            <div className="activity__description">{ activity.description }</div>
-                            <div className="activity__datetime">{ activity.date_time }</div>
-                            <div className="activity__address">{ activity.address }</div>
-                            <a className="activity__btn" href={`/cities/${activity.city_id}/${activity.category_id}/${activity.id}`}>Detail</a>
-                            
-                        </div>
-                    ))
-                }
-            </div>  
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d163931.25644248963!2d14.32553874982573!3d50.05958535668972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b939c0970798b%3A0x400af0f66164090!2sPraha!5e0!3m2!1scs!2scz!4v1594736640583!5m2!1scs!2scz" width="600" height="450" frameBorder="0"  allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>   
+                                <div className="activity" key={ activity.id }>
+                                    
+                                   <a href={`/cities/${activity.city_id}/${activity.category_id}/${activity.id}`}> <div className="activity__name"  >{ activity.name }</div></a> 
+                                    <div className="activity__description">{ activity.description }</div>
+                                    <div className="activity__datetime">{ activity.date_time }</div>
+                                    <div className="activity__address">{ activity.address }</div>
+                                    {/* 
+                                    <a className="activity__btn" href={`/cities/${activity.city_id}/${activity.category_id}/${activity.id}`}>Detail</a>
+                                     */}
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>  
+                
+                <div>     
+                <iframe className="activity-list-detail__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d163931.25644248963!2d14.32553874982573!3d50.05958535668972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b939c0970798b%3A0x400af0f66164090!2sPraha!5e0!3m2!1scs!2scz!4v1594736640583!5m2!1scs!2scz" width="600" height="450" frameBorder="0"  allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>   
+                </div> 
+            </div>
+             
             </>                                
         )
     }
