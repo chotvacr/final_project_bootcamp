b26bef58c812a54c5ccc89f4c70e972e15d8bc94
Richard Chotvac <chotvacr@gmail.com>
2020-07-16 11:39:52 +0200

    login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,13 +7,13 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+                    <div class="card-header"></div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
-                            <div class="form-group row">
+                            <div class="form-grouprow">
                                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                                 <div class="col-md-6">
@@ -27,7 +27,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-grouprow">
                                 <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
 
                                 <div class="col-md-6">
@@ -41,7 +41,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-grouprow">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
@@ -55,7 +55,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-grouprow">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
@@ -69,7 +69,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-grouprow">
                                 <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                                 <div class="col-md-6">
@@ -79,7 +79,7 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btnbtn-primary">
                                         {{ __('Register') }}
                                     </button>
                                 </div>
